859f07cd3a90ca35e6ca5a5ad34ff4219d61ff8f
stijn meersman <stijn.meersman@vaultspeed.com>
2023-09-06 16:51:16 +0200

    generify FMC setup

--- examples/fmc_setup.py
+++ examples/fmc_setup.py
@@ -1,4 +1,6 @@
+import argparse
 import logging
+import os
 from datetime import datetime, timezone
 
 from vaultspeed_sdk.client import Client, UserPasswordAuthentication
@@ -6,57 +8,78 @@ from vaultspeed_sdk.fmc import FlowTypes
 from vaultspeed_sdk.models.metadata.load_type import LoadTypes
 from vaultspeed_sdk.system import System
 
-import env as env
-
 """
 This script can create FMC flows for a Data Vault, this is useful when you have a lot of sources, 
 so that you don't have to create an init and incr flow for each of them.
 """
 
-"""
- Preparation
-"""
-logging.basicConfig(level=logging.INFO)
-authentication = UserPasswordAuthentication(api_url=env.base_url, username=env.user_name, password=env.password)
-client = Client(base_url=env.base_url, auth=authentication, retries=1, caller="examples")
-system = System(client=client)
-project = system.get_project("moto")
-data_vault = project.get_data_vault(name="moto_no_af")
 
-for flow in data_vault.fmc_flows:
-    data_vault.delete_fmc_flow(flow)
+def main(project_name: str, data_vault_name: str):
+    """
+     Preparation
+    """
+    logging.basicConfig(level=logging.INFO)
+    auth = UserPasswordAuthentication(api_url=os.environ.get("VS_URL"), username=os.environ.get("VS_USER"),
+                                      password=os.environ.get("VS_PASSWORD"))
+    client = Client(base_url=os.environ.get("VS_URL"), auth=auth, retries=1, caller="examples")
+    system = System(client=client)
+    project = system.get_project(project_name)
+    data_vault = project.get_data_vault(name=data_vault_name)
 
-"""
- create FMC flows
-"""
-for load_type in [LoadTypes.INIT, LoadTypes.INCR]:
-    for source in project.sources:
-        if source.build_flag and source.name in ("moto_sales", "moto_mktg"):
-            data_vault.create_fmc_flow(
-                name=f"{source.name}_{load_type.value.lower()}",
-                description=f"{source.name}_{load_type.value.lower()}",
-                start_date=datetime.now(timezone.utc).replace(minute=0, hour=0, second=0, microsecond=0),
-                concurrency=4,
-                flow_type=FlowTypes.FL,
-                load_type=load_type,
-                group_tasks=False,
-                dv_connection_name="sf",
-                source=source,
-                schedule_interval="\"@hourly\"",
-                src_connection_name="src",
-                etl_connection_name="dbt"
-            )
+    for flow in data_vault.fmc_flows:
+        data_vault.delete_fmc_flow(flow)
 
-    data_vault.create_fmc_flow(
-        name=f"{data_vault.code}_bv_{load_type.value.lower()}",
-        description=f"{data_vault.code}_bv_{load_type.value.lower()}",
-        start_date=datetime.now(timezone.utc).replace(minute=0, hour=0, second=0, microsecond=0),
-        concurrency=4,
-        flow_type=FlowTypes.BV,
-        load_type=load_type,
-        group_tasks=False,
-        dv_connection_name="dv",
-        schedule_interval="timedelta(hours=1)"
+    """
+     create FMC flows
+    """
+    for load_type in [LoadTypes.INIT, LoadTypes.INCR]:
+        for source in project.sources:
+            if source.build_flag:
+                data_vault.create_fmc_flow(
+                    name=f"{source.name}_{load_type.value.lower()}",
+                    description=f"{source.name}_{load_type.value.lower()}",
+                    start_date=datetime.now(timezone.utc).replace(minute=0, hour=0, second=0, microsecond=0),
+                    concurrency=4,
+                    flow_type=FlowTypes.FL,
+                    load_type=load_type,
+                    group_tasks=False,
+                    dv_connection_name="sf",
+                    source=source,
+                    schedule_interval="\"@hourly\"",
+                    src_connection_name="src"
+                )
+
+        data_vault.create_fmc_flow(
+            name=f"{data_vault.code}_bv_{load_type.value.lower()}",
+            description=f"{data_vault.code}_bv_{load_type.value.lower()}",
+            start_date=datetime.now(timezone.utc).replace(minute=0, hour=0, second=0, microsecond=0),
+            concurrency=4,
+            flow_type=FlowTypes.BV,
+            load_type=load_type,
+            group_tasks=False,
+            dv_connection_name="dv",
+            schedule_interval="timedelta(hours=1)"
+        )
+
+    print(data_vault.fmc_flows)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="Create FMC Flows",
+        description="""
+        This script can create FMC flows for a Data Vault, this is useful when you have a lot of sources, 
+        so that you don't have to create an init and incr flow for each of them.
+        """
     )
+    parser.add_argument(
+        "project",
+        help="Name of the Project"
+    )
+    parser.add_argument(
+        "data_vault",
+        help="Name of the Data Vault"
+    )
+    args = parser.parse_args()
 
-print(data_vault.fmc_flows)
+    main(args.project, args.data_vault)
