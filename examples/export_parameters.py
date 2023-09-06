d308978d532d630ad215286fccbcc4794f409920
stijn meersman <stijn.meersman@vaultspeed.com>
2023-09-06 16:09:28 +0200

    added parameter export example

---
+++ examples/export_parameters.py
@@ -0,0 +1,45 @@
+import argparse
+import csv
+import os
+from pathlib import Path
+
+from vaultspeed_sdk.client import Client, UserPasswordAuthentication
+from vaultspeed_sdk.system import System
+
+
+def main(project: str, csv_path: Path):
+    auth = UserPasswordAuthentication(api_url=os.environ.get("VS_URL"), username=os.environ.get("VS_USER"),
+                                      password=os.environ.get("VS_PASSWORD"))
+    client = Client(base_url=os.environ.get("VS_URL"), auth=auth, retries=1, caller="examples")
+    system = System(client=client)
+
+    p = system.get_project(project)
+
+    with open(csv_path) as csvfile:
+        writer = csv.writer(csvfile, delimiter=",")
+        header = ["name", "value", "type", "description"]
+        writer.writerow(header)
+        for param in p.parameters:
+            data = [param.name, param.value, param.type, param.description]
+            writer.writerow(data)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="Export Project Parameters",
+        description="Exports the parameters from a project to a CSV file."
+    )
+    parser.add_argument(
+        "project",
+        help="Name of the Project.",
+    )
+    parser.add_argument(
+        "CSV path",
+        help="path of the CSV file to export to.",
+        dest="csv_path",
+        action="store",
+        type=Path
+    )
+    args = parser.parse_args()
+
+    main(args.project, args.csv_path)
